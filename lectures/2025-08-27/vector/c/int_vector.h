58e470408739fcebe43650181a412fdaa703673c
Erik Pihl <erik_pihl_91@hotmail.com>
2025-08-27 10:18:54 +0200

    Add vector implementation

---
+++ lectures/2025-08-27/vector/c/int_vector.h
@@ -0,0 +1,61 @@
+/**
+ * @brief Int vector implementation.
+ */
+#pragma once
+
+#include <stddef.h> // For size_t.
+/**
+ * @brief Create a bool enumeration just for demonstration.
+ */
+typedef enum { false, true } bool;
+
+/** Int vector structure. */
+typedef struct int_vector int_vector_t;
+
+/**
+ * @brief Create a new empty vector.
+ * 
+ * @return Pointer to the new vector, or NULL if the vector couldn't be created.
+ */
+int_vector_t* int_vector_new(void);
+
+/**
+ * @brief Delete the vector and set the associated pointer to NULL.
+ * 
+ * @param[in] self Double pointer to the vector.
+ */
+void int_vector_delete(int_vector_t** self);
+
+
+/**
+ * @brief Get the data of the vector.
+ * 
+ * @param[in] self Pointer to the vector in question.
+ * 
+ * @return Pointer to the data of the vector.
+ */
+const int* int_vector_data(const int_vector_t* self);
+
+/**
+ * @brief Get the size of the vector.
+ * 
+ * @param[in] self Pointer to the vector in question.
+ * 
+ * @return The size of the vector.
+ */
+size_t int_vector_size(const int_vector_t* self);
+
+/**
+ * @brief Clear the vector.
+ * 
+ * @param[in] self Pointer to the vector to clear.
+ */
+void int_vector_clear(int_vector_t* self);
+
+/**
+ * @brief Push an element to the back of the vector.
+ * 
+ * @param[in] self Pointer to the vector in question.
+ * @param[in] element The new element to push.
+ */
+bool int_vector_push_back(int_vector_t* self, const int element);
--- lectures/2025-08-27/vector/c/main.c
+++ lectures/2025-08-27/vector/c/main.c
@@ -3,31 +3,25 @@
  */
 #include <stdio.h>
 
-/**
- * @brief Structure holding car parameters.
- */
-typedef struct car
-{
-    const char* brand;             // The car brand.
-    const char* model;            // The car model.
-    unsigned year_of_manufacture; // The year of manufacture.
-} car_t;
-
-/**
- * @brief Print information about the given car.
- */
-void car_print(const car_t* self)
-{
-    printf("%s %s, manufactured %u\n", self->brand, self->model, 
-        self->year_of_manufacture);
-}
+#include "int_vector.h"
 
 int main(void)
 {
-    car_t car1 = {"Volvo", "V70", 1991U};
-    car_t car2 = {"BMW", "Z3", 2003U};
+    // Create a new vector.
+    int_vector_t* v1 = int_vector_new();
 
-    car_print(&car1);
-    car_print(&car2);
+    // Push ten integers to the back or the vector.
+    for (int i = 0; i < 10; ++i)
+    {
+        int_vector_push_back(v1, i);
+    }
+
+    for (size_t i = 0U; i < int_vector_size(v1); ++i)
+    {
+        printf("%d\n", int_vector_data(v1)[i]);
+    }
+
+    // Free allocated memory to avoid memory leaks.
+    int_vector_delete(&v1);
     return 0;
 }
